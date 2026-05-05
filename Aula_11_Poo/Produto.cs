46f43c40af12dc74d62bf9561598ec88be6d3d57
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-05-05 19:54:51 -0300

    feat: add exercicio 1 aula 11 - array de Produto

---
+++ Aula_11_Poo/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula_11_Poo
+{
+    class Produto
+    {
+        public decimal Valor { get; set; }
+        public string Nome { get; set; }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"O produto {Nome} custa {Valor}.");
+            Console.WriteLine();
+        }
+
+        public static void Atividade1()
+        {
+            Produto[] produtos = new Produto[]
+            {
+                new Produto() { Nome = "Notebook", Valor = 3000 },
+                new Produto() { Nome = "Smartphone", Valor = 8000 },
+                new Produto() { Nome = "Tablet", Valor = 1500 }
+            };
+
+
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                produtos[i].ExibirInformacoes();
+            }
+        }
+    }
+}
