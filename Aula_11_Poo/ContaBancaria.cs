0abd86e9096ba7ca1bf513c4e61e7dbb0a0e9c57
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-05-07 11:17:21 -0300

    refactor: move logica das atividades para Program sem static

--- Aula_11_Poo/ContaBancaria.cs
+++ Aula_11_Poo/ContaBancaria.cs
@@ -7,13 +7,13 @@ namespace Aula_11_Poo
     internal class ContaBancaria
     {
         #region Propriedades
-
         public decimal Valor { get; set; }
         public string Titular { get; set; }
 
         #endregion
 
         #region Metodos
+
         public void Depositar(string mensagem)
         {
             Console.WriteLine(mensagem);
@@ -66,106 +66,8 @@ namespace Aula_11_Poo
             Console.WriteLine();
         }
 
-        public static void CadastrarConta(ContaBancaria[] contas, ref int contador)
-        {
-            Console.WriteLine("==== CADASTRO DE CONTA BANCARIA ==== ");
-            if (contador >= contas.Length)
-            {
-                Console.WriteLine("Limite de contas atingido. Não é possível cadastrar mais contas.");
-                return;
-            }
-
-            Console.Write("Digite o nome do titular da conta: ");
-            contas[contador] = new ContaBancaria();
-            contas[contador].Titular = Console.ReadLine();
-            while (string.IsNullOrWhiteSpace(contas[contador].Titular))
-            {
-                Console.WriteLine("Nome do titular não pode ser vazio. Digite novamente:");
-                contas[contador].Titular = Console.ReadLine();
-            }
-            contas[contador].Valor = 0;
-            contador++;
-        }
-
         #endregion
 
-        #region Atividades
-
-        public static void Atividade4()
-        {
-            ContaBancaria[] contas = new ContaBancaria[2];
-            contas[0] = new ContaBancaria()
-            {
-                Titular = "João",
-                Valor = 30000
-            };
-
-            bool sair = false;
-            while (!sair)
-            {
-                Console.WriteLine("==== DIGITE UMA OPCAO ====");
-                Console.WriteLine("1 - Depositar");
-                Console.WriteLine("2 - Sacar");
-                Console.WriteLine("3 - Sair");
-                string opcao = Console.ReadLine();
-                switch (opcao)
-                {
-                    case "1":
-                        Console.Clear();
-                        contas[0].Depositar($"Digite o valor a ser depositado na conta de {contas[0].Titular}:");
-                        contas[0].ExibirInformacoes();
-                        Program.PausaParaLer();
-                        break;
-                    case "2":
-                        Console.WriteLine();
-                        contas[0].Sacar($"Digite o valor a ser sacado da conta de {contas[0].Titular}:");
-                        contas[0].ExibirInformacoes();
-                        Program.PausaParaLer();
-                        break;
-                    case "3":
-                        sair = true;
-                        break;
-                }
-            }
-        }
-
-        public static void Atividade9()
-        {
-            ContaBancaria[] contas = new ContaBancaria[1];
-            int contador = 0;
-            int indiceConta = 0;
-            bool sair = false;
-            while (!sair)
-            {
-                Console.WriteLine("==== DIGITE UMA OPCAO ====");
-                Console.WriteLine("1 - Criar Conta");
-                Console.WriteLine("2 - Depositar");
-                Console.WriteLine("3 - Sacar");
-                Console.WriteLine("4 - Exibir Saldo");
-                Console.WriteLine("5 - Sair");
-                string opcao = Console.ReadLine();
-                switch (opcao)
-                {
-                    case "1":
-                        CadastrarConta(contas, ref contador);
-                        break;
-                    case "2":
-                        contas[indiceConta].Depositar($"Digite o valor a ser depositado na conta de {contas[indiceConta].Titular}:");
-                        break;
-                    case "3":
-                        contas[indiceConta].Sacar($"Digite o valor a ser sacado da conta de {contas[indiceConta].Titular}:");
-                        break;
-                    case "4":
-                        contas[indiceConta].ExibirInformacoes();
-                        break;
-                    case "5":
-                        sair = true;
-                        break;
-                }
-            }
-        }
-
-        #endregion
     }
 }
 
