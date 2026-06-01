6edbe99c973c9ab7271d37a79bae0d0ca7fc9ed7
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-01 20:55:15 -0300

    feat: adiciona atividades de Coleções e LINQ - Aula 16

---
+++ Aula_16/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula_16
+{
+    internal class Pessoa
+    {
+        private string _nome;
+        public string Nome 
+        { 
+            get { return _nome; } 
+            set { _nome = value; } 
+        }
+
+        public Pessoa (string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
