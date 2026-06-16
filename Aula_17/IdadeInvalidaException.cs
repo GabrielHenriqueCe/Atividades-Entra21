f2d3d2c003543f461008142b2f340f840cfa4409
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-16 20:06:50 -0300

    feat: feitas atividades da aula 17 (#23)

---
+++ Aula_17/IdadeInvalidaException.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula_17
+{
+    class IdadeInvalidaException : Exception
+    {
+        public IdadeInvalidaException(int idade)
+            : base($"\nIdade inválida: {idade} anos\n") { }
+    }
+}
---
+++ Aula_17/SaldoInsuficienteException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula_17
+{
+    internal class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException(decimal saldo, decimal saque)
+    : base($"\nValor solicitado de R${saque:F2} é superior ao saldo Atual R${saldo:F2}\n") { }
+    }
+}
