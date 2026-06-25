ea4649794a1dfa957c01a604fc1c9e24f0d1672e
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-25 20:51:13 -0300

    Modulo 9 Aula 20: projeto MVC (MeuSite) com SaudacaoController e roteamento (#27)

---
+++ Aula_20/Controllers/SaudacaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSite.Controllers
+{
+    public class SaudacaoController : Controller
+    {
+        public IActionResult Ola(string nome)
+        {
+            return Content($"Olá, {nome}! Bem-vindo ao ASP.NET Core.");
+        }
+    }
+}
