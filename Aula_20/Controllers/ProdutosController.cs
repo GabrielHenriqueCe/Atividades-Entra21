6bcd4b4aab2b6cb6b3399e7b4ff86c9990f98868
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-25 21:16:01 -0300

    feat: ProdutosController com action Index retornando lista de produtos em JSON (#28)

---
+++ Aula_20/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula_20.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> produtos = new List<string>
+            {
+                "Mouse",
+                "Teclado",
+                "Monitor",
+                "Webcam",
+                "Headset"
+            };
+
+            return Json(produtos);
+        }
+    }
+}
