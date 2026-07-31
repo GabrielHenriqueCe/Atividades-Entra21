9c84008ac7aa0d42849a1d2622e5e27a12695999
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-07-30 23:53:01 -0300

    Adiciona models Alunos, Curso, Livro e Emprestimo com EF Core

---
+++ MVC/Models/Curso.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+    }
+}
