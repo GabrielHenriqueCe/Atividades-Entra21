f96b163298d2156a95b7b89f58265cd60826f92a
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-08-05 19:47:26 -0300

    feat: ajustes de pastas

--- Modulo_09/ASP.NET Core MVC completo/Models/Produto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AspNetCoreMvcCompleto.Models
-{
-    public class Produto
-    {
-        public int ID { get; set; }
-        public string Nome { get; set; }
-        public decimal Preco { get; set; }
-    }
-}
