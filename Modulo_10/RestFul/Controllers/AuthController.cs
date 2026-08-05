830223040c9d7bdfdf0482d5415e8a5b4194f609
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-08-05 19:41:58 -0300

    feat: ajustes de pastas e atividades do modulo 10

---
+++ Modulo_10/RestFul/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly TokenService _tokenService;
+
+    public AuthController(TokenService ts) => _tokenService = ts;
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest login)
+    {
+        if (login.Usuario != "admin" || login.Senha != "123456")
+            return Unauthorized();
+
+        var token = _tokenService.GerarToken(login.Usuario);
+        return Ok(new { token });
+    }
+}
---
+++ Modulo_10/RestFul/Services/TokenService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+public class TokenService 
+{
+    private readonly string _chave;
+
+    public TokenService(IConfiguration config)
+        => _chave = config["Jwt:Key"];
+
+    public string GerarToken(string usuario)
+    {
+        var claims = new[] { new Claim(ClaimTypes.Name, usuario) };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_chave));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddHours(2),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
